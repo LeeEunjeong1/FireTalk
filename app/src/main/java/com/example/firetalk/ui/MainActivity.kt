735abbc95f2710fb6aa06488a28fab3637bddd85
Eunjeong <tbig1019@gmail.com>
2022-03-15 18:19:37 +0900

    main bottom menu 생성

--- app/src/main/java/com/example/firetalk/ui/MainActivity.kt
+++ app/src/main/java/com/example/firetalk/ui/MainActivity.kt
@@ -3,10 +3,35 @@ package com.example.firetalk.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firetalk.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bnv_main = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bnv_main.run { setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.tab1 -> {
+                    // 다른 프래그먼트 화면으로 이동하는 기능
+                    val restFragment = Fragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, restFragment).commit()
+                }
+                R.id.tab2 -> {
+                    val searchFragment = Fragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, searchFragment).commit()
+                }
+                R.id.tab3 -> {
+                    val reservationFragment = Fragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, reservationFragment).commit()
+                }
+            }
+            true
+        }
+            selectedItemId = R.id.tab1
+        }
     }
 }
