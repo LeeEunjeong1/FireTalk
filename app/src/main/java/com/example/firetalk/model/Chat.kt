9dfe259ed7c5097972d8bcaeb062b81007cfe13f
Eunjeong <tbig1019@gmail.com>
2022-03-17 12:54:09 +0900

    chat - 채팅방 화면 구성

---
+++ app/src/main/java/com/example/firetalk/model/Chat.kt
@@ -0,0 +1,11 @@
+package com.example.firetalk.model
+
+class Chat(
+    val users: HashMap<String, Boolean> = HashMap(),
+    val comments: HashMap<String, Comment> = HashMap())
+{
+    class Comment(
+        val uid: String? = null,
+        val message: String? = null,
+        val time : String? = null)
+}
