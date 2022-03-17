e5c53a16b6c30a844c461f2a944526d190dd7b49
Eunjeong <tbig1019@gmail.com>
2022-03-17 16:19:03 +0900

    chatting - intent값 받아오기

--- app/src/main/java/com/example/firetalk/ui/ChattingActivity.kt
+++ app/src/main/java/com/example/firetalk/ui/ChattingActivity.kt
@@ -3,15 +3,25 @@ package com.example.firetalk.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firetalk.databinding.ActivityChattingBinding
-
+import com.example.firetalk.utils.UserPreferences
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChattingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChattingBinding
+
+    private lateinit var database : DatabaseReference
+    private var friendUid : String? = null
+    private var myUid : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityChattingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        friendUid = intent.getStringExtra("uid")
+        myUid = UserPreferences.id
     }
 }
