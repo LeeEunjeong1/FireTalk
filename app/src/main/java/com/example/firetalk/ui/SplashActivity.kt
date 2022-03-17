fa668865bc15a509cd979fd40be4705c7e334cf1
Eunjeong <tbig1019@gmail.com>
2022-03-17 12:00:37 +0900

    Splash - 자동로그인 기능 추가

--- app/src/main/java/com/example/firetalk/ui/SplashActivity.kt
+++ app/src/main/java/com/example/firetalk/ui/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firetalk.databinding.ActivitySplashBinding
+import com.example.firetalk.ui.main.MainActivity
 import com.example.firetalk.utils.UserPreferences
 
 class SplashActivity :AppCompatActivity() {
@@ -17,16 +18,27 @@ class SplashActivity :AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        UserPreferences.init(this)
+
         Handler(Looper.getMainLooper()).postDelayed({
+            isUser()
+        },2000)
+
+
+    }
+
+    private fun isUser(){
+        if(UserPreferences.id != ""){
+            val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }else{
             val intent = Intent(this,LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        },2000)
-
-        UserPreferences.init(this)
-
-
+        }
     }
 
 
