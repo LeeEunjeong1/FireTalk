805778f80bdebba7dcc82d621a926fcfec4c5949
Eunjeong <tbig1019@gmail.com>
2022-03-18 13:03:39 +0900

    chatting - 채팅기능 추가

--- app/src/main/java/com/example/firetalk/ui/ChattingActivity.kt
+++ app/src/main/java/com/example/firetalk/ui/ChattingActivity.kt
@@ -1,20 +1,31 @@
 package com.example.firetalk.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.firetalk.databinding.ActivityChattingBinding
+import com.example.firetalk.model.Chat
+import com.example.firetalk.model.Friend
 import com.example.firetalk.utils.UserPreferences
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChattingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChattingBinding
+    private var adapter = ChattingAdapter()
 
-    private lateinit var database : DatabaseReference
+    private lateinit var fireDatabase : DatabaseReference
     private var friendUid : String? = null
     private var myUid : String? = null
+    private var chatRoomUid : String ? = null
 
+    private var friend : Friend? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,5 +34,97 @@ class ChattingActivity : AppCompatActivity() {
 
         friendUid = intent.getStringExtra("uid")
         myUid = UserPreferences.id
+
+
+        fireDatabase = Firebase.database.reference
+        initListener()
+    }
+
+    private fun initListener(){
+        //메세지를 보낸 시간
+        val time = System.currentTimeMillis()
+        val dateFormat = SimpleDateFormat("MM월dd일 hh:mm", Locale.getDefault())
+        val curTime = dateFormat.format(Date(time)).toString()
+
+        with(binding){
+            imageView.setOnClickListener{
+                val chatModel = Chat()
+                chatModel.users[myUid.toString()] = true
+                chatModel.users[friendUid!!] = true
+
+                val comment = Chat.Comment(myUid, sendMsg.text.toString(), curTime)
+                if(chatRoomUid == null){
+                    imageView.isEnabled = false
+                    fireDatabase.child("chatrooms").push().setValue(chatModel).addOnSuccessListener {
+                        //채팅방 생성
+                        checkChatRoom()
+                        //메세지 보내기
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").push().setValue(comment)
+                            sendMsg.text = null
+                        },1000L)
+                    }
+                }else{
+                    fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").push().setValue(comment)
+                    sendMsg.text = null
+                }
+            }
+            checkChatRoom()
+        }
+    }
+
+    private fun checkChatRoom(){
+        fireDatabase.child("chatrooms").orderByChild("users/$myUid").equalTo(true)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for(item in snapshot.children){
+                        val chatModel = item.getValue<Chat>()
+                        if(chatModel?.users!!.containsKey(friendUid)){
+                            chatRoomUid = item.key
+                            binding.imageView.isEnabled = true
+                            getFriendInfo()
+                        }
+                    }
+                }
+            })
+    }
+    private fun getFriendInfo(){
+        binding.recyclerView.adapter = adapter
+        fireDatabase.child("users").child(friendUid.toString()).addListenerForSingleValueEvent(
+            object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    friend = snapshot.getValue<Friend>()
+                    val friendImage = friend?.image
+                    val friendName = friend?.name
+                    binding.name.text = friend?.name
+                    getMessageList(friendImage!!,friendName!!)
+                }
+
+
+            })
+    }
+    private fun getMessageList(friendImage: String,friendName:String){
+        fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                adapter.clearList()
+                for(data in snapshot.children){
+                    val item = data.getValue<Chat.Comment>()
+                    adapter.setChattingList(item!!)
+                }
+                adapter.setFriend(friendImage,friendName)
+                binding.recyclerView?.scrollToPosition(adapter.itemCount-1)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/firetalk/ui/ChattingAdapter.kt
@@ -0,0 +1,87 @@
+package com.example.firetalk.ui
+
+import android.content.Intent
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.firetalk.R
+import com.example.firetalk.databinding.ItemChatBinding
+import com.example.firetalk.model.Friend
+import com.example.firetalk.databinding.ItemFriendBinding
+import com.example.firetalk.databinding.ItemMessageBinding
+import com.example.firetalk.model.Chat
+import com.example.firetalk.ui.ChattingActivity
+import com.example.firetalk.utils.UserPreferences
+import com.google.firebase.database.DatabaseReference
+
+class ChattingAdapter: RecyclerView.Adapter<ChattingViewHolder>() {
+    private lateinit var fireDatabase : DatabaseReference
+    private var comments = mutableListOf<Chat.Comment>()
+    private var friend: Friend? = null
+    private var friendImage : String? = null
+    private var friendName : String? = null
+
+    fun  clearList(){
+        comments.clear()
+    }
+    fun setChattingList(chat: Chat.Comment) {
+        comments.add(chat)
+        notifyDataSetChanged()
+    }
+    fun setFriend(friendImg : String,friendNa: String){
+        friendImage = friendImg
+        friendName = friendNa
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChattingViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMessageBinding.inflate(inflater, parent, false)
+        return ChattingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ChattingViewHolder, position: Int) {
+        (holder as? ChattingViewHolder)?.onBind(comments[position])
+        Glide.with(holder.itemView.context)
+            .load(friendImage)
+            .apply(RequestOptions().circleCrop())
+            .into(holder.image_profile)
+        holder.friend_name.text = friendName
+    }
+
+    override fun getItemCount(): Int {
+        return comments.size
+    }
+}
+
+class ChattingViewHolder(private val binding: ItemMessageBinding) : RecyclerView.ViewHolder(binding.root) {
+    val image_profile : ImageView = binding.profileImage
+    val friend_name : TextView = binding.name
+
+    fun onBind(item: Chat.Comment){
+        with(binding){
+            message.textSize = 20F
+            message.text = item.message
+            timeStamp.text = item.time
+            //본인 채팅
+            if(item.uid.equals(UserPreferences.id)){
+                message.setBackgroundResource(R.drawable.my_bubble)
+                name.visibility = View.INVISIBLE
+                profileImage.visibility = View.INVISIBLE
+                layout.gravity = Gravity.RIGHT
+            }else{
+                layout.visibility = View.VISIBLE
+                name.visibility = View.VISIBLE
+                message.setBackgroundResource(R.drawable.friend_bubble)
+                layout.gravity = Gravity.LEFT
+            }
+        }
+    }
+
+}
+
