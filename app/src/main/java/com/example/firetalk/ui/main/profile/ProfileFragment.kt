af71293b03a7e561ad5c5d5332e1f2a0e5c0c681
Eunjeong <tbig1019@gmail.com>
2022-03-21 18:20:52 +0900

    profile 사진, 이름 변경 (에러 수정 필요)

--- app/src/main/java/com/example/firetalk/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/firetalk/ui/main/profile/ProfileFragment.kt
@@ -1,17 +1,30 @@
 package com.example.firetalk.ui.main.profile
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.firetalk.databinding.FragmentMainProfileBinding
+import com.example.firetalk.model.Friend
 import com.example.firetalk.ui.LoginActivity
 import com.example.firetalk.utils.UserPreferences
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 
 class ProfileFragment : Fragment() {
 
@@ -19,12 +32,21 @@ class ProfileFragment : Fragment() {
     private val binding get() = lBinding!!
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var fireDatabase : DatabaseReference
+    private var imageUri : Uri? = null
+
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
         lBinding = FragmentMainProfileBinding.inflate(inflater, container, false)
 
         auth = Firebase.auth
+        fireDatabase = Firebase.database.reference
+
+        imageChange = false
+
+        initLayout()
         initListener()
+
         return binding.root
 
     }
@@ -38,8 +60,28 @@ class ProfileFragment : Fragment() {
         lBinding = null
     }
 
+    private fun initLayout(){
+        //프로필 값 넣기
+        fireDatabase.child("users").child(UserPreferences.id).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) { }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userProfile = snapshot.getValue<Friend>()
+                Glide.with(requireContext())
+                    .load(userProfile?.image)
+                    .apply(
+                        RequestOptions()
+                        .circleCrop())
+                    .into(binding.imageView)
+                binding.edtId.text = userProfile?.email
+                binding.edtName.setText(userProfile?.name)
+            }
+        })
+
+    }
+
     private fun initListener(){
         with(binding){
+            //로그아웃 버튼
             btnLogout.setOnClickListener {
                 UserPreferences.logout()
                 auth.signOut()
@@ -50,7 +92,60 @@ class ProfileFragment : Fragment() {
                 activity?.finish()
 
             }
+            //프로필사진 클릭했을때
+            imageView.setOnClickListener {
+                val intentImage = Intent(Intent.ACTION_PICK)
+                intentImage.type = MediaStore.Images.Media.CONTENT_TYPE
+                getResult.launch(intentImage)
+            }
+            //저장하기 버튼
+            btnProfile.setOnClickListener {
+                if(edtName.text.isNotEmpty()){
+                    //이름변경
+                    fireDatabase.child("users/${UserPreferences.id}/name").setValue(edtName.text.toString())
+                    edtName.clearFocus()
+                    if(imageChange){
+                        //프로필 사진 변경
+                        
+                        FirebaseStorage.getInstance().reference
+                            .child("userImage/${UserPreferences.id}/photo").delete().addOnSuccessListener {
+                                FirebaseStorage.getInstance().reference.child("userImage/${UserPreferences.id}/photo").putFile(imageUri!!).addOnSuccessListener {
+                                    FirebaseStorage.getInstance().reference.child("userImage/${UserPreferences.id}/photo").downloadUrl.addOnSuccessListener {
+                                        val photoUri : Uri = it
+                                        Log.d("profileImage",it.toString())
+                                        fireDatabase.child("users").child(UserPreferences.id).child("image").setValue(photoUri.toString())
+
+                                        Toast.makeText(context,"변경되었습니다.",Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                            }
+                    }
+                }else{
+                    Toast.makeText(context,"이름을 입력해주세요.",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+    }
+
+    private var getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == AppCompatActivity.RESULT_OK){
+            // 이미지 원본 경로
+            imageUri = it.data?.data
+            // 이미지뷰  변경
+            Glide.with(requireContext())
+                .load(imageUri)
+                .apply(
+                    RequestOptions()
+                        .circleCrop())
+                .into(binding.imageView)
+            imageChange = true
+        }else{
+            Toast.makeText(context,"다시 시도해주세요.",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    companion object{
+        var imageChange : Boolean =false
     }
 
 }
