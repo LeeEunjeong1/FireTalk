cb54dc0f09440c2936604a567b8dd3ee987ec0cf
Eunjeong <tbig1019@gmail.com>
2022-03-21 16:25:41 +0900

    로그아웃 기능 구현

--- app/src/main/java/com/example/firetalk/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/firetalk/ui/main/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.firetalk.ui.main.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,15 +8,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.firetalk.databinding.FragmentMainProfileBinding
+import com.example.firetalk.ui.LoginActivity
+import com.example.firetalk.ui.main.MainActivity
+import com.example.firetalk.utils.UserPreferences
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
     private var lBinding: FragmentMainProfileBinding? = null
     private val binding get() = lBinding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
         lBinding = FragmentMainProfileBinding.inflate(inflater, container, false)
 
+        auth = Firebase.auth
+        initListener()
         return binding.root
 
     }
@@ -27,6 +38,21 @@ class ProfileFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         lBinding = null
+    }
+
+    private fun initListener(){
+        with(binding){
+            btnLogout.setOnClickListener {
+                UserPreferences.logout()
+                auth.signOut()
+                val intent = Intent(requireContext(), LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                activity?.finish()
+               
+            }
+        }
     }
 
 }
