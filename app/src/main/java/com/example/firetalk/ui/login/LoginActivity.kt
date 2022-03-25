fea8b71c0d55b9fa6807118592f74ca5ba1c5f0d
Eunjeong <tbig1019@gmail.com>
2022-03-25 14:12:40 +0900

    viewModel 적용 - 로그인

--- app/src/main/java/com/example/firetalk/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/firetalk/ui/login/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.firetalk.ui
+package com.example.firetalk.ui.login
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,10 +7,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.firetalk.R
 import com.example.firetalk.databinding.ActivityLoginBinding
 import com.example.firetalk.model.User
+import com.example.firetalk.ui.Signup.SignupActivity
 import com.example.firetalk.ui.main.MainActivity
+import com.example.firetalk.ui.main.profile.ProfileViewModel
 import com.example.firetalk.utils.UserPreferences
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -33,8 +36,14 @@ class LoginActivity :AppCompatActivity() {
     private lateinit var client: GoogleSignInClient
     lateinit var database: DatabaseReference
 
+
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        observeViewModel()
 
         //firebase.auth
         auth = Firebase.auth
@@ -46,18 +55,31 @@ class LoginActivity :AppCompatActivity() {
         initBinding()
     }
 
+    private fun observeViewModel(){
+        val intentMain = Intent(this, MainActivity::class.java)
+        with(viewModel){
+           isLoginSuccess.observe(this@LoginActivity){
+               if(it == true){
+                   startActivity(intentMain)
+                   finish()
+               }else{
+                   Toast.makeText(applicationContext,"아이디 또는 비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
+               }
+           }
+        }
+    }
+
     private fun initBinding(){
-        val signupIntent = Intent(this,SignupActivity::class.java)
+        val signupIntent = Intent(this, SignupActivity::class.java)
         with(binding){
             btnLogin.setOnClickListener{
                 if(edtId.text.isEmpty() && edtPwd.text.isEmpty()){
                     Toast.makeText(applicationContext,"아이디 또는 비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show()
                 }else{
-                    doLogin(edtId.text.toString(), edtPwd.text.toString())
+                    viewModel.doLogin(edtId.text.toString(), edtPwd.text.toString(),this@LoginActivity)
                 }
             }
             btnSignup.setOnClickListener {
-
                 startActivity(signupIntent)
                 Log.d("here","here")
             }
@@ -70,24 +92,6 @@ class LoginActivity :AppCompatActivity() {
         }
     }
 
-
-    private fun doLogin(id: String, pwd : String){
-        val intentMain = Intent(this, MainActivity::class.java)
-
-        auth.signInWithEmailAndPassword(id,pwd)
-            .addOnCompleteListener(this){task->
-                if(task.isSuccessful){
-                    Log.d("Login",task.toString())
-                    UserPreferences.id = auth.currentUser!!.uid
-                    finish()
-                    startActivity(intentMain)
-                }else{
-                    Toast.makeText(applicationContext,"아이디 또는 비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
-                    Log.d("Login 실패",task.toString())
-                    UserPreferences.id = ""
-                }
-            }
-    }
     public override fun onStart(){
         super.onStart()
         val user = auth.currentUser
--- app/src/main/java/com/example/firetalk/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/firetalk/ui/login/LoginViewModel.kt
@@ -1,4 +1,52 @@
 package com.example.firetalk.ui.login
 
-class LoginViewModel {
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.firetalk.ui.main.MainActivity
+import com.example.firetalk.utils.UserPreferences
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
+
+class LoginViewModel: ViewModel() {
+    private var auth: FirebaseAuth = Firebase.auth
+    private var fireDatabase : DatabaseReference = Firebase.database.reference
+
+    private var isLoginSuccess1 = MutableLiveData<Boolean>()
+    val isLoginSuccess get() = isLoginSuccess1
+
+    var error1 = MutableLiveData<String>()
+    val error get() = error1
+
+    fun doLogin(id: String, pwd : String, activity:Activity){
+        CoroutineScope(Dispatchers.Default).launch {
+            try{
+                auth.signInWithEmailAndPassword(id,pwd)
+                    .addOnCompleteListener(activity){ task->
+                        if(task.isSuccessful){
+                            UserPreferences.id = auth.currentUser!!.uid
+                            isLoginSuccess1.postValue(true)
+                        }else{
+                            UserPreferences.id = ""
+                            isLoginSuccess1.postValue(false)
+                        }
+                    }
+            }catch (e:Exception){
+                error1.postValue(e.message)
+            }
+        }
+
+    }
+
+
 }
