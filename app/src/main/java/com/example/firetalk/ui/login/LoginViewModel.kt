4282080cd53758df43712325517ec58458d3cc1f
Eunjeong <tbig1019@gmail.com>
2022-03-28 13:11:53 +0900

    login viewmodel 안쓰는  import 삭제

--- app/src/main/java/com/example/firetalk/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/firetalk/ui/login/LoginViewModel.kt
@@ -1,26 +1,18 @@
 package com.example.firetalk.ui.login
 
 import android.app.Activity
-import android.content.Intent
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.firetalk.ui.main.MainActivity
 import com.example.firetalk.utils.UserPreferences
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlin.coroutines.coroutineContext
 
 class LoginViewModel: ViewModel() {
     private var auth: FirebaseAuth = Firebase.auth
-    private var fireDatabase : DatabaseReference = Firebase.database.reference
 
     private var isLoginSuccess1 = MutableLiveData<Boolean>()
     val isLoginSuccess get() = isLoginSuccess1
